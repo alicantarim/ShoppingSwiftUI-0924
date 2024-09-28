0c7d43c37ca35b8dabf93381bf2d65dd020d829c
Alican Tarım <alicantarim@hotmail.com>
2024-09-28 13:42:10 +0300

    Initial Commit

---
+++ ShoppingSwiftUI/ShoppingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingSwiftUIApp.swift
+//  ShoppingSwiftUI
+//
+//  Created by Alican TARIM on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
